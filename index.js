79f88282f3737f868c9b2595e4e1c5864a59e77d
mehedi66 <mh5203280@gmail.com>
2025-11-11 22:23:30 +0600

    booking section

--- index.js
+++ index.js
@@ -75,6 +75,16 @@ async function run() {
       res.send(result);
     });
 
+    app.patch("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: { status: "unavailable" },
+      };
+      const result = await carCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
     app.put("/cars/:id", async (req, res) => {
       const id = req.params.id;
       const updateData = req.body;
